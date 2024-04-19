af0944ca4ede381ade3b38296260ba931f8c5478
RDDcat <yukmaro@gmail.com>
2024-04-19 16:42:39 +0900

    [ADD] 타이틀 네브 생성 및 로고 추가

---
+++ src/components/TitleNav.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="title">
+        <div class="title-logo">
+            <img src="@/assets/logo.png">
+            Pick&LOL
+        </div>
+    </div>
+</template>
+<script>
+
+
+export default {
+    components: {
+
+    },
+    data(){
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
+<style scoped>
+.title{
+    
+}
+.title-logo img{
+    width: 3rem;
+    height: 3rem;
+}
+
+
+</style>
