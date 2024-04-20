7e5faddded001c01ce07923c193c17cb5c7bfceb
RDDcat <yukmaro@gmail.com>
2024-04-20 17:43:38 +0900

    [ADD] 샘플 모달 컴포넌트 추가

---
+++ src/components/modal/SampleModal.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="absolute bg-red-950 w-screen h-screen">
+
+    </div>
+</template>
+<script>
+
+
+export default {
+    components: {
+
+    },
+    data(){
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
+<style scoped>
+
+
+
+</style>
