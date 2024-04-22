1c5e1ca391c205a4ef6720bc2e60d331d48202b3
RDDcat <yukmaro@gmail.com>
2024-04-22 19:55:13 +0900

    [UPDATE] 로그인 모달 컴포넌트 업데이트

--- src/components/modal/LoginModal.vue
+++ src/components/modal/LoginModal.vue
@@ -1,6 +1,16 @@
 <template>
-    <div class="absolute bg-red-950 w-screen h-screen">
-        로그인
+    <div class="absolute bg-red-950 opacity-90 w-screen h-screen flex">
+        <!--  -->
+        <div class="m-auto w-96 h-96">
+            <!-- 상단 -->
+            <div class="bg-red-600 rounded-t-xl w-full h-16">
+                로그인
+            </div>
+            <!-- 하단 -->
+            <div class="bg-white rounded-b-xl w-full h-16">
+
+            </div>
+        </div>        
     </div>
 </template>
 <script>
