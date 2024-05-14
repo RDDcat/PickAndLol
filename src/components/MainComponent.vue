9750e3651a0bb1e870bd89ce2355d231e876e88b
RDDcat <yukmaro@gmail.com>
2024-05-14 18:06:13 +0900

    [UPDATE] Main 컴포넌트 요소 업데이트

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,5 +1,18 @@
 <template>
-    <div class="sample">this page for copy</div>
+    <!-- 선수단 등록 페이지 -->
+    <div class="w-full flex flex-col">
+        <!-- 이미지 컨테이너 (지도좌표) -->
+        <div class="relative flex mx-auto w-[50rem]">
+            <img class="mx-auto" src="@/assets/map.png" alt="">
+        </div>
+
+        <!-- 선수단 네비 -->
+
+        <!-- 선수 카드 -->
+        <div class="w-24 h-32">
+
+        </div>
+    </div>
 </template>
 <script>
 
