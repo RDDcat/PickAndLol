2df0d8b8531f0af1d9a551694532304e897f4a89
RDDcat <yukmaro@gmail.com>
2024-05-15 16:20:56 +0900

    [UPDATE] 맵 컴포넌트 이미지 변경 및 hover 이벤트 추가

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -4,11 +4,11 @@
         <!-- 이미지 컨테이너 (지도좌표) -->
         <div class="relative flex mx-auto">
             <img class="mx-auto" src="@/assets/map.png" alt="">
-            <img class="absolute left-10 top-10" src="@/assets/top.png">
-            <img class="absolute left-28 top-40" src="@/assets/jug.png">
-            <img class="absolute left-56 top-56" src="@/assets/mid.png">
-            <img class="absolute right-32 bottom-20" src="@/assets/bot.png">
-            <img class="absolute right-8 bottom-8" src="@/assets/sup.png">
+            <img class="absolute left-10 top-10 hover:ring-4 hover:ring-red-400 rounded-full" src="@/assets/top.png">
+            <img class="absolute left-28 top-40 hover:ring-4 hover:ring-red-400 rounded-full" src="@/assets/jug.png">
+            <img class="absolute left-56 top-56 hover:ring-4 hover:ring-red-400 rounded-full" src="@/assets/mid.png">
+            <img class="absolute right-32 bottom-20 hover:ring-4 hover:ring-red-400 rounded-full" src="@/assets/bot.png">
+            <img class="absolute right-8 bottom-8 hover:ring-4 hover:ring-red-400 rounded-full" src="@/assets/sup.png">
         </div>
 
         <!-- 선수단 네비 -->
@@ -37,7 +37,6 @@ export default {
 }
 </script>
 <style scoped>
-
 
 
 </style>
