99c6004182f069f1837eb6efde36d17e482d8978
RDDcat <yukmaro@gmail.com>
2024-05-15 15:40:00 +0900

    [UPDATE] 메인 컴포넌트 지도에 라이별 이미지 업데이트

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -2,8 +2,13 @@
     <!-- 선수단 등록 페이지 -->
     <div class="w-full flex flex-col">
         <!-- 이미지 컨테이너 (지도좌표) -->
-        <div class="relative flex mx-auto w-[50rem]">
+        <div class="relative flex mx-auto">
             <img class="mx-auto" src="@/assets/map.png" alt="">
+            <img class="absolute left-10 top-10" src="@/assets/top.png">
+            <img class="absolute left-28 top-40" src="@/assets/jug.png">
+            <img class="absolute left-56 top-56" src="@/assets/mid.png">
+            <img class="absolute right-32 bottom-20" src="@/assets/bot.png">
+            <img class="absolute right-8 bottom-8" src="@/assets/sup.png">
         </div>
 
         <!-- 선수단 네비 -->
