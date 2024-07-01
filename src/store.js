bf8738508014c8eb8fd2466229436c1ec3829bea
RDDcat <yukmaro@gmail.com>
2024-07-01 11:23:29 +0900

    [UPDATE] LOGO 해상도 업데이트

--- src/store.js
+++ src/store.js
@@ -3,17 +3,19 @@ import VuexPersistence from 'vuex-persist';
 
 const vuexLocal = new VuexPersistence({
   storage: window.localStorage,
-  modules: ['modalStore', 'cacheStore'],
+  modules: ['modalStore', 'cacheStore', 'staticStore'],
 });
 
 // 작성한 모듈을 가져오기
 import cacheStore from '@/store/cacheStore.js';
 import modalStore from '@/store/modalStore.js';
+import staticStore from '@/store/staticStore.js';
 const store = new Vuex.Store({
   
   modules: {
     cacheStore,
     modalStore,
+    staticStore,
   },
   plugins: [vuexLocal.plugin],
 });
---
+++ src/store/staticStore.js
@@ -0,0 +1,22 @@
+// Vuex 스토어를 정의하여 모달과 관련된 상태를 관리
+const staticStore = {
+  state: {
+
+
+  },
+
+  getters: {
+
+  },
+  
+  mutations: {
+    
+  },
+
+  actions: {
+
+  }
+};
+
+// staticStore 내보냄
+export default staticStore;
