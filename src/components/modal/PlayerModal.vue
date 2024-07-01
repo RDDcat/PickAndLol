4b01d974baea56947cb8cd5e067b92358a6e5f41
RDDcat <yukmaro@gmail.com>
2024-07-01 18:49:36 +0900

    [UPDATE] 선수 모달 배경 투명도 추가

--- src/components/modal/PlayerModal.vue
+++ src/components/modal/PlayerModal.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="fixed overscroll-contain bg-red-950 flex w-full h-screen z-40" @click="this.$store.state.modalStore.isPlayerModal=false">
+    <div class="fixed overscroll-contain bg-opacity-90 bg-red-950 flex w-full h-screen z-40" @click="this.$store.state.modalStore.isPlayerModal=false">
         <img class="mx-auto w-80 object-contain" src="@/assets/coming-soon-logo.png">
     </div>
 </template>
