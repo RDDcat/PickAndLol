f66dd121c3b288e2efe0b91699da3907086d5fa4
RDDcat <yukmaro@gmail.com>
2024-07-04 15:35:03 +0900

    [UPDATE] modalStore 주석 추가

--- src/store/modalStore.js
+++ src/store/modalStore.js
@@ -3,12 +3,15 @@ import { defineStore } from 'pinia'
 // use + 스토어 이름 << 컨벤션임
 export const useModalStore = defineStore('modalStore', {
   state: ()=>({
-    // 로그인 창
+    //모달
     isLoginModal: false,
     isLoginWarnModal: false,
     isGuideModal: false,
     isSelectCaptainModal: false,
+    // 선수 분석 모달
     isPlayerModal: false,
+    selectPlayer:'',
+    // 경고창 모달
     isWarningModal: false,
     warningFrom:'',
     warningTitle:'title',
