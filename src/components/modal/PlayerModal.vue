05ded3ad5f6b49520738c9457f9453cea02b6ae5
RDDcat <yukmaro@gmail.com>
2024-07-04 15:23:01 +0900

    [UPDATE] 모달에 가이드 텍스트 추가

--- src/components/modal/PlayerModal.vue
+++ src/components/modal/PlayerModal.vue
@@ -1,9 +1,13 @@
 <template>
 <div class="fixed overscroll-contain bg-opacity-90 bg-red-950 flex w-full h-screen z-40" @click.stop="this.modalStore.isPlayerModal=false">
     <!-- 전체 프레임 -->
-    <div class="relative mx-auto w-[1080px] flex" @click.stop>
+    <div class="relative mx-auto w-[1080px] flex flex-col" @click.stop>
+        <!-- 안내 문구 -->
+        <div class="mx-auto mt-8 text-gray-200 opacity-20">
+            배경 클릭시 꺼짐
+        </div>
         <!-- 윗 프레임 -->
-        <div class="relative mx-auto mt-20 w-full h-[700px] flex">
+        <div class="relative mx-auto mt-8 w-full h-[700px] flex">
             <!-- 큰 선수 이미지 -->
             <img class="absolute left-4 bottom-0 w-[400px] object-contain" :src="modalStore.selectPlayer.img">
             <!-- 왼쪽 프레임 -->
@@ -14,9 +18,8 @@
             <div class="w-[700px] h-full bg-white rounded-r-lg">
 
             </div>
-
         </div>
-    </div>        
+    </div>
     <!-- <img class="mx-auto w-80 object-contain" src="@/assets/coming-soon-logo.png"> -->
 </div>
 </template>
