c36c3114e69c6da69df02aae6c7bad9ea82b2330
RDDcat <yukmaro@gmail.com>
2024-07-04 18:54:13 +0900

    [UPDATE] 계정 로그아웃 -> 로그아웃으로 변경

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -59,7 +59,7 @@
                     </button>
                     <button @click="isLogout()"
                         type="button" class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 ">
-                        계정 로그아웃
+                        로그아웃
                     </button>
                 </div>
             </div>
