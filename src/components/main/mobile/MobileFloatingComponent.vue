354f834b7f406c2da82342c741b0c17753779e79
RDDcat <yukmaro@gmail.com>
2024-08-02 13:59:55 +0900

    [FIX] 네비게이션 반대로 연결되어있는것 수정

--- src/components/main/mobile/MobileFloatingComponent.vue
+++ src/components/main/mobile/MobileFloatingComponent.vue
@@ -3,11 +3,11 @@
         <!-- 응원팀으로 가기 -->
         <div class="flex mb-1 w-10 h-10 bg-point-600 rounded-l-lg">
             <img class="m-auto w-8 h-8 p-0.5 rounded-full bg-white object-contain" :src="cacheStore.myTeam.teamLogo" 
-                @click="modalStore.isMobilePlayerListModal=true"/>
+                @click="modalStore.isMobileSelectClubModal=true"/>
         </div>
         <!-- 선수 리스트로 가기 -->
         <div class="flex flex-col w-10 py-6 bg-gray-700 rounded-l-lg" 
-            @click="modalStore.isMobileSelectClubModal=true">
+            @click="modalStore.isMobilePlayerListModal=true">
             <div class="text-white mx-auto [writing-mode:vertical-rl]">
                 선수리스트
             </div>
