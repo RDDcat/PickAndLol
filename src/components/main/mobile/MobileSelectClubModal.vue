60832ff6ba910ab3e1040dd6adcac6cfe283ee1d
RDDcat <yukmaro@gmail.com>
2024-08-02 14:07:58 +0900

    [UPDATE] 모바일 팀선택 모달 하단 버튼 컴포넌트 추가

--- src/components/main/mobile/MobileSelectClubModal.vue
+++ src/components/main/mobile/MobileSelectClubModal.vue
@@ -6,7 +6,20 @@
             <img class="w-6 h-6 object-contain" src="@/assets/arrow_back_black.svg" @click.prevent="modalStore.isMobileSelectClubModal=false">
         </div>
 
-        팀선택
+        <!-- 가이드 텍스트 -->
+
+        <!-- 팀 선택 스크롤 -->
+
+        <!-- 하단고정 -->
+        <div class="fixed bottom-4 flex flex-col w-full px-6">
+            <!-- 태그 -->
+            <div class="mx-auto px-5 py-1.5 text-sm rounded-full bg-gray-200 mb-2">
+                TAG
+            </div>
+            <!-- 버튼 -->
+            <button class="w-full bg-point-600 text-white text-lg py-1.5 rounded-lg">다음</button>
+
+        </div>
     </div>
 </template>
 <script>
