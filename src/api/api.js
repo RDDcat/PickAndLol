ff4b5bcbe1dd210d20ed208f01ffc69791004867
RDDcat <yukmaro@gmail.com>
2024-08-07 13:30:32 +0900

    [FIX] api 헤더에 access-token 추가

--- src/api/api.js
+++ src/api/api.js
@@ -45,6 +45,7 @@ export default {
         return axios.post(BASE_URL + 'member', body,{
             headers: {
                 withCredentials: true,
+                'access-token': cacheStore.accessToken,
             },
         });
     },
